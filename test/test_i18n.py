671287b82868d4a221a62117f6ae5648e3adf333
Kevin Howell <khowell@redhat.com>
2018-07-18 14:29:06 -0400

    kitchen replacement WIP

--- test/test_i18n.py
+++ test/test_i18n.py
@@ -5,9 +5,24 @@ try:
 except ImportError:
     import unittest
 
+import codecs
+import glob
+from kitchen.text.display import textual_width as kitchen_textual_width
+
 from subscription_manager.i18n import configure_i18n
+from subscription_manager.unicode_width import textual_width
 
 
 class TestI18N(unittest.TestCase):
     def test_configure_i18n(self):
         configure_i18n()
+
+    def test_text_width(self):
+        for po_file in glob.glob('po/*.po'):
+            with codecs.open(po_file, 'r', encoding='utf-8') as translation_file:
+                for number, line in enumerate(translation_file.readlines(), 1):
+                    expected = kitchen_textual_width(line)
+                    actual = textual_width(line)
+                    self.assertEqual(actual, expected, msg='mismatch on line {} of file {}, {} vs. {}'.format(
+                         number, po_file, expected, actual
+                    ))
